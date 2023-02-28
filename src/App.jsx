0fb0299edaafcb05c55a099a583ead830ef3cd86
Maryam Khan <maryam.khan@pcc.edu>
2023-02-27 19:32:49 -0800

    Added preliminary styling and created intro text in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>100 FREE, Quick Dev Crash Courses</h1>
+      <p>
+        Got some free time? Check out these free courses on various web and
+        software dev technologies. Pick up a new skill (or grow an existing one)
+        in under 90 minutes!
       </p>
     </div>
-  )
-}
-
-export default App
+  );
+};
+export default App;
